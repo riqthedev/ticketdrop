177a720de544000986575c2fc50b66ad6282843d
riqthedev <riqthedev@gmail.com>
2025-11-19 22:21:09 -0500

    Move API source code out of api/ directory to fix Vercel function limit

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 // Vercel serverless function entry point
 // Import the app directly - Vercel will handle TypeScript compilation
-import app from './src/index';
+import app from '../src/index';
 
 // Export as default for Vercel serverless functions
 export default app;
